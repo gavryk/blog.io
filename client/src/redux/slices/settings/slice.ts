e8e0db288403b480551cc05a837a94705da22dbe
Gavryk <gvozdyk23@gmail.com>
2022-12-05 06:05:43 +0200

    ui: Prepeared Project for development

---
+++ client/src/redux/slices/settings/slice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { SettingsSliceTypes } from './types';
+
+const initialState: SettingsSliceTypes = {
+  isLoaded: true,
+};
+
+export const settingsSlice = createSlice({
+  name: 'settings',
+  initialState,
+  reducers: {
+    setLoading: (state, action) => {
+      state.isLoaded = action.payload;
+    },
+  },
+});
+
+export const { setLoading } = settingsSlice.actions;
+
+export default settingsSlice.reducer;
