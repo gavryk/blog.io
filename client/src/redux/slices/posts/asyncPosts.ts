8c4fcebed105b2e422e215084f4dd567cb51ae51
Gavryk <gvozdyk23@gmail.com>
2022-12-10 04:27:25 +0200

    client: chenge redux status loadin types, finished post card rfc

--- client/src/redux/slices/posts/asyncPosts.ts
+++ client/src/redux/slices/posts/asyncPosts.ts
@@ -5,3 +5,8 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   const { data } = await axios.get(`/posts`);
   return data;
 });
+
+export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
+  const { data } = await axios.get(`/tags`);
+  return data;
+});
