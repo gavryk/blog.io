c4fbbb0bbb20e02841061357804b142db2746e95
Gavryk <gvozdyk23@gmail.com>
2022-12-25 03:37:57 +0200

    filtering posts functionality

--- client/src/widgets/sort/index.tsx
+++ client/src/widgets/sort/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { UIDropdown } from '../../components';
+import { useSelector } from 'react-redux';
+import { UIDropdown, UITypography } from '../../components';
+import { postsSelector } from '../../redux/slices/posts/selector';
 import { SortTypes } from '../../redux/slices/posts/types';
 import { sortList } from './model';
 import styles from './styles.module.scss';
@@ -10,8 +12,13 @@ interface SortWidgetProps {
 }
 
 export const SortWidget: React.FC<SortWidgetProps> = ({ sortBy, onSort }) => {
+  const { filterBy } = useSelector(postsSelector);
+
   return (
     <div className={styles.root}>
+      <UITypography variant="h3" bottomSpace="none" fontWeight="bold">
+        {filterBy === '' ? 'All Posts' : `#${filterBy}`}
+      </UITypography>
       <UIDropdown list={sortList} selected={sortBy} onSetSort={onSort} />
     </div>
   );
